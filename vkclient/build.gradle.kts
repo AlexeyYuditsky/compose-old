aab480e9ed7c95c1a27c0786ed8eff5152b6bd59
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-02-21 08:57:46 +0300

    Compose

--- vkclient/build.gradle.kts
+++ vkclient/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("io.coil-kt:coil-compose:2.1.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- vkclient/src/main/java/com/alexeyyuditsky/vkclient/MainActivity.kt
+++ vkclient/src/main/java/com/alexeyyuditsky/vkclient/MainActivity.kt
@@ -3,6 +3,7 @@ package com.alexeyyuditsky.vkclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.alexeyyuditsky.vkclient.ui.ActivityResultTest
 import com.alexeyyuditsky.vkclient.ui.MainScreen
 import com.alexeyyuditsky.vkclient.ui.theme.VkClientTheme
 
@@ -10,6 +11,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { VkClientTheme { MainScreen() } }
+        setContent { VkClientTheme { ActivityResultTest() } }
     }
 }
