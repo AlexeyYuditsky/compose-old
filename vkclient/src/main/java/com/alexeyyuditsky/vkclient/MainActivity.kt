5b2fb63474c14c9067103365dc4f3a4d397366d6
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-03-05 09:46:46 +0300

    Compose

--- vkclient/src/main/java/com/alexeyyuditsky/vkclient/MainActivity.kt
+++ vkclient/src/main/java/com/alexeyyuditsky/vkclient/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.alexeyyuditsky.vkclient.core.log
 import com.alexeyyuditsky.vkclient.ui.ActivityResultTest
 import com.alexeyyuditsky.vkclient.ui.MainScreen
 import com.alexeyyuditsky.vkclient.ui.theme.VkClientTheme
@@ -17,21 +24,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkClientTheme {
+                val someValue = remember { mutableStateOf(true) }
+
+                log("Recomposition ${someValue.value}")
                 val launcher = rememberLauncherForActivityResult(
                     contract = VK.getVKAuthActivityResultContract()
                 ) {
                     when (it) {
                         is VKAuthenticationResult.Success -> {
+                            log("Success")
                             // User passed authorization
                         }
 
                         is VKAuthenticationResult.Failed -> {
+                            log("Failed")
                             // User didn't pass authorization
                         }
                     }
                 }
-                launcher.launch(listOf(VKScope.WALL))
-                MainScreen()
+                LaunchedEffect(key1 = Unit) {
+                    log("LaunchedEffect")
+                }
+                SideEffect {
+                    log("SideEffect")
+                    //launcher.launch(listOf(VKScope.WALL))
+                }
+                //MainScreen()
+                Button(onClick = { someValue.value = !someValue.value }) {
+                    Text(text = "Change state")
+                }
             }
         }
     }
---
+++ vkclient/src/main/java/com/alexeyyuditsky/vkclient/ui/theme/SideEffectTest.kt
@@ -0,0 +1,41 @@
+package com.alexeyyuditsky.vkclient.ui.theme
+
+import android.os.Handler
+import android.os.Looper
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.alexeyyuditsky.vkclient.core.log
+
+@Composable
+fun SideEffectTest(number: MyNumber) {
+    Handler(Looper.getMainLooper()).post{
+        number.a = 2
+        log("change")
+    }
+
+    Column {
+        LazyColumn {
+            items(500) {
+                log("item")
+                Text(text = number.a.toString())
+            }
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        LazyColumn {
+            items(500) {
+                log("item")
+                Text(text = number.a.toString())
+            }
+        }
+    }
+}
+
+data class MyNumber(var a: Int)
