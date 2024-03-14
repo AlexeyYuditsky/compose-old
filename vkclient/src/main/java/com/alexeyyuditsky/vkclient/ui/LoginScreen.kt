469f52418390929ee7467d93ef183c4811a136e9
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-03-14 09:53:54 +0300

    Add authorization

--- vkclient/src/main/java/com/alexeyyuditsky/vkclient/ui/LoginScreen.kt
+++ vkclient/src/main/java/com/alexeyyuditsky/vkclient/ui/LoginScreen.kt
@@ -2,13 +2,11 @@ package com.alexeyyuditsky.vkclient.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -18,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alexeyyuditsky.vkclient.R
 import com.alexeyyuditsky.vkclient.ui.theme.DarkBlue
@@ -27,30 +24,25 @@ import com.alexeyyuditsky.vkclient.ui.theme.DarkBlue
 fun LoginScreen(
     onLoginClick: () -> Unit
 ) {
-    Box(
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Column(
-            modifier = Modifier.wrapContentHeight(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Image(
+            modifier = Modifier.size(100.dp),
+            painter = painterResource(id = R.drawable.ic_vk),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.height(100.dp))
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = DarkBlue,
+                contentColor = Color.White
+            ),
+            onClick = { onLoginClick.invoke() }
         ) {
-            Image(
-                modifier = Modifier.size(100.dp),
-                painter = painterResource(id = R.drawable.ic_vk),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.height(100.dp))
-            Button(
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = DarkBlue,
-                    contentColor = Color.White
-                ),
-                onClick = { onLoginClick.invoke() }
-            ) {
-                Text(text = stringResource(R.string.login))
-            }
+            Text(text = stringResource(R.string.login))
         }
     }
 }
